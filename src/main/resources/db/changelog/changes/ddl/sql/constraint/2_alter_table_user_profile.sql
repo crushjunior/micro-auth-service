ef906b9e005b37f04e607852291a09fc6001f540
Vadim Charushnikov <charushnikovvadim@gmail.com>
2024-04-16 18:34:40 +0300

    Добавил миграцию через ликвибейз

---
+++ src/main/resources/db/changelog/changes/ddl/sql/constraint/2_alter_table_user_profile.sql
@@ -0,0 +1,2 @@
+CREATE UNIQUE INDEX user_profile_email_key
+    ON user_profile (email);
---
+++ src/main/resources/db/changelog/changes/ddl/sql/rollback/1_drop_tables.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS refresh_token;
+DROP TABLE IF EXISTS token;
+DROP TABLE IF EXISTS user_profile;
+DROP TABLE IF EXISTS client;
+DROP TABLE IF EXISTS passport_data;
