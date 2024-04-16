9c6f024c1ef22c295f514e17de8d5a0259f7eedc
Vadim Charushnikov <charushnikovvadim@gmail.com>
2024-04-16 14:17:18 +0300

    Initial commit

---
+++ src/main/java/ru/charushnikov/microauthservice/MicroAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.charushnikov.microauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroAuthServiceApplication.class, args);
+    }
+
+}
