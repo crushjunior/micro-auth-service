bc7f1f39b8daf5d8eb8f32450e0ee0670d23867c
Vadim Charushnikov <charushnikovvadim@gmail.com>
2024-04-18 16:49:42 +0300

    добавил свагер

--- src/main/java/ru/charushnikov/microauthservice/controller/RegistrationController.java
+++ src/main/java/ru/charushnikov/microauthservice/controller/RegistrationController.java
@@ -1,7 +1,14 @@
 package ru.charushnikov.microauthservice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.api.ErrorMessage;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.charushnikov.microauthservice.model.dto.request.RegisterRequestDto;
@@ -16,17 +23,17 @@ public class RegistrationController {
     private final RegistrationService registrationService;
 
     @PostMapping("/user-profile/new")
-//    @Operation(summary = "EP1: Register client in application", tags = "registration",
-//            responses = {
-//                    @ApiResponse(responseCode = "200", description = "Success",
-//                            headers = @Header(name = "Set-Cookie",
-//                                    description = "Required if Is-Web-Request = true. May not appear in Swagger UI"),
-//                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
-//                                    schema = @Schema(implementation = RegisterResponseClientDto.class))),
-//                    @ApiResponse(responseCode = "400", description = "Bad request",
-//                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
-//                                    schema = @Schema(implementation = ErrorMessage.class)))
-//            })
+    @Operation(summary = "EP1: Register client in application", tags = "registration",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Success",
+                            headers = @Header(name = "Set-Cookie",
+                                    description = "Required if Is-Web-Request = true. May not appear in Swagger UI"),
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = RegisterResponseDto.class))),
+                    @ApiResponse(responseCode = "400", description = "Bad request",
+                            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = ErrorMessage.class)))
+            })
     public ResponseEntity<RegisterResponseDto> registerClientInApp(@Valid @RequestBody RegisterRequestDto dto) {
         RegisterResponseDto registerRequestClientDto = registrationService.registerClientInApp(dto);
         return ResponseEntity.ok(registerRequestClientDto);
