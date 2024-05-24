c1d21c4769f624a59cc6d71f584499f69875fa91
Vadim Charushnikov <charushnikovvadim@gmail.com>
2024-05-24 12:17:24 +0300

    добавил feign

--- src/main/java/ru/charushnikov/microauthservice/MicroAuthServiceApplication.java
+++ src/main/java/ru/charushnikov/microauthservice/MicroAuthServiceApplication.java
@@ -2,8 +2,10 @@ package ru.charushnikov.microauthservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class MicroAuthServiceApplication {
 
     public static void main(String[] args) {
